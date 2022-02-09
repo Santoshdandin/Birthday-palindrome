059ea7d45b9a3ef7b23b9fc563e8d389eaea66e0
Santoshdandin <santusb427@gmail.com>
2022-02-10 00:16:04 +0530

    half

--- app.js
+++ app.js
@@ -38,5 +38,37 @@ function convertDateToStr(date) {
     return dateStr
 }
 
+function getAllDateFormats(date) {
+    var dateStr = convertDateToStr(date);
 
+    var ddmmyyyy =  dateStr.day + dateStr.month + dateStr.year; 
+    var mmddyyyy = dateStr.month + dateStr.day + dateStr.year;
+    var yyyymmdd = dateStr.year + dateStr.month + dateStr.day;
+    var ddmmyy = dateStr.day + dateStr.month + dateStr.year.slice(-2);
+    var mmddyy = dateStr.month + dateStr.day + dateStr.year.slice(-2);
+    var yymmdd = dateStr.year.slice(-2) + dateStr.month + dateStr.day;
 
+    return [ddmmyyyy, mmddyyyy, yyyymmdd, ddmmyy, mmddyy, yymmdd];
+}
+
+function checkPalindromeForAllDateFormats(date) {
+    var listOfPalindromes = getAllDateFormats(date);
+
+    var flag = false;
+
+    for (var i=0; i<listOfPalindromes.length; i++) {
+        if(isPalindrome(listOfPalindromes[i])) {
+            flag = true;
+            break;
+        }
+    }
+    return flag;
+}
+
+var date = {
+    day: 2,
+    month: 11,
+    year: 2020
+};
+
+console.log(checkPalindromeForAllDateFormats(date));
